ddcc7efc7ec7117df540fce6f75525550ea40c51
Meng3www <alexmengde@live.com>
2026-07-01 19:14:07 +0200

    brutus

--- python_scripts/hyperparameter_tuning.py
+++ python_scripts/hyperparameter_tuning.py
@@ -1,26 +1,30 @@
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
+"""
+https://www.geeksforgeeks.org/machine-learning/hyperparameter-tuning/
+GridSearchCV 
+for each layer, get the steering vector
+    for coeff in range(1, 21):
+        steer 10 prompts with the vector and coeff
+        count the number of success
+return the layer, coeff of the max count
+alternatives: RandomizedSearchCV and Bayesian Optimization
+"""
+
+# from sentence_transformers import SentenceTransformer
+# from sklearn.metrics.pairwise import cosine_similarity
 from transformers import pipeline
 from transformer_lens.model_bridge import TransformerBridge
-import ctypes, ctypes.util
-import gc
+# import ctypes, ctypes.util
 import json
 import torch
 import transformer_lens.utilities as utils
 
 working_dir = "/scratch/fmeng/ActAdd/data_result/"
 path_siebert = "/scratch/common_models/SiEBERT/"
-path_all_MiniLM = "/scratch/common_models/all-MiniLM-L6-v2/"
 path_Llama3 = "/scratch/common_models/Meta-Llama-3-8B/"
 path_opt = "/scratch/common_models/opt-6.7b/"
 
 device = utils.get_device()
 print("device:", device)
-
-
-# https://stackoverflow.com/questions/51938963/python-memory-not-being-released-on-linux
-def malloc_trim():
-    ctypes.CDLL(ctypes.util.find_library('c')).malloc_trim(0) 
 
 def prompts2tokens(prompt_add, prompt_sub, model):
     """
@@ -48,20 +52,15 @@ def hooked_generate(prompts, editing_hooks, model, seed=0, **kwargs):
         result = model.generate(input=prompts, max_new_tokens=64, do_sample=True, **kwargs)
     return result
 
-def save2file(data2save, file_name):
-    with open(f"{working_dir}imdb_{file_name}_sen_rel.json", "w") as f:
-        json.dump(data2save, f, skipkeys=True)
-    print(f"file saved as {working_dir}imdb_{file_name}_sen_rel.json")
-
-def steer_prompts(prompt_add, prompt_sub, prompts, steer_model, sentiment_model, relevance_model, layer, coeff, seed, sampling_kwargs, file_name):
-    steered_all = list()
+def steer_prompts(prompt_add, prompt_sub, prompts, steer_model, sentiment_model, layer, seed, sampling_kwargs, grid, max_coeff):
+    """
+    grid: torch.Tensor of size (model_layer, 20)
+    """
     # get the steering vector
     tokens_add, tokens_sub = prompts2tokens(prompt_add, prompt_sub, steer_model)
     act_add = tokens2resid_pre(tokens_add, layer, steer_model)
     act_sub = tokens2resid_pre(tokens_sub, layer, steer_model)
     act_diff = act_add - act_sub
-    act_diff = act_diff * coeff
-
     def add_activation(activation, hook):
         if activation.shape[1] == 1: return
         prompt_dim, steering_dim = activation.shape[1], act_diff.shape[1]
@@ -70,31 +69,25 @@ def steer_prompts(prompt_add, prompt_sub, prompts, steer_model, sentiment_model,
         except:
             print(f"More mod tokens ({steering_dim}) than prompt tokens ({prompt_dim})!")
 
-    # generate with the steering vector
-    editing_hooks = [(f"blocks.{layer}.hook_resid_pre", add_activation)]
-    for prompt in prompts:
-        steering_case = {"prompt": prompt}
-        generated_text = hooked_generate(prompt, editing_hooks, steer_model, seed, **sampling_kwargs)
-        steering_case["generated_text"] = generated_text  
-        # continuation_label
-        sentiment_result = sentiment_model(generated_text[len(prompt):])
-        if sentiment_result[0]["label"] == "POSITIVE":
-            steering_case["continuation_label"] = 1
-        else:  
-            steering_case["continuation_label"] = 0
-        # similarity
-        embedding_prompt = relevance_model.encode(prompt)
-        embedding_generated_text = relevance_model.encode(generated_text[len(prompt):])
-        relevance = cosine_similarity(embedding_prompt.reshape(1, -1), 
-                                    embedding_generated_text.reshape(1, -1))
-        steering_case["similarity"] = relevance[0][0].item()
-        steered_all.append(steering_case)
-    print(len(steered_all), " documents steered")
-    save2file(steered_all, file_name)
+    for coeff in range(1, max_coeff):
+        act_diff = act_diff * coeff     
+        # generate with the steering vector
+        editing_hooks = [(f"blocks.{layer}.hook_resid_pre", add_activation)]
+        sentiments = list() 
+        for prompt in prompts:
+            generated_text = hooked_generate(prompt, editing_hooks, steer_model, seed, **sampling_kwargs)
+            # continuation_label
+            sentiment_result = sentiment_model(generated_text[len(prompt):])
+            if sentiment_result[0]["label"] == "POSITIVE":
+                sentiments.append(1)
+            else:  
+                sentiments.append(0)
+        grid[layer, coeff-1] = sum(sentiments)
+    return grid
 
 
 if __name__ == '__main__':
-    layer, coeff, seed = 6, 5, 0
+    seed, max_coeff = 0, 20
     sampling_kwargs = dict(temperature=1.0, top_p=1.0, freq_penalty=0.0)
     # load steering model
     model_steer = TransformerBridge.boot_transformers(path_Llama3, device=device)
@@ -103,21 +96,26 @@ if __name__ == '__main__':
     # load sentiment model
     model_sentiment = pipeline("sentiment-analysis", model=path_siebert)
     print("sentiment model loaded")
-    # load relevance model
-    model_relevance = SentenceTransformer(path_all_MiniLM)
-    print("relevance model loaded")
     # load data
     with open(f"{working_dir}imdb_neg.json", "r") as f:
         prompts_neg = json.load(f)
+    prompts_neg = prompts_neg[:10]
     print("imdb_neg.json loaded")
-    steer_prompts(prompt_add=" love", 
-                  prompt_sub=" hate", 
-                  prompts=prompts_neg[:3], 
-                  steer_model=model_steer, 
-                  sentiment_model=model_sentiment, 
-                  relevance_model=model_relevance, 
-                  layer=layer, 
-                  coeff=coeff, 
-                  seed=seed, 
-                  sampling_kwargs=sampling_kwargs, 
-                  file_name="neg2pos")
+    # check the number of layers
+    n_layers = model_steer.cfg.n_layers
+    print(f"number of layers: {n_layers}")
+    # init grid
+    grid = torch.zeros(n_layers, max_coeff)
+    for layer in range(n_layers):
+        grid = steer_prompts(prompt_add=" love", 
+                            prompt_sub=" hate", 
+                            prompts=prompts_neg, 
+                            steer_model=model_steer, 
+                            sentiment_model=model_sentiment, 
+                            layer=layer, 
+                            seed=seed, 
+                            sampling_kwargs=sampling_kwargs, 
+                            grid=grid,
+                            max_coeff=max_coeff)
+        print(grid)
+        break
